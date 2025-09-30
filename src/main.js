54c33d98bd390b5a9d7cf98f4d4310101261a987
Roflan74 <apodshivalov74ru@gmail.com>
2025-09-30 09:49:58 +0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-const card = document.querySelector(".card");
-
-card.addEventListener("click", () => {
-  if (card.classList.contains("card-show")) {
-    card.classList.remove("card-show");
-    } else {
-    card.classList.add("card-show");
-  }
-})
+const cards = document.querySelectorAll(".card");
+for (let card of cards) {
+    card.addEventListener("click", () => {
+        if (card.classList.contains("card-show")) {
+            card.classList.remove("card-show");
+        } else {
+            card.classList.add("card-show");
+        }
+    })
+}
